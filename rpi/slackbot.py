353a57cf65dcce3b1a46f0543fe620ec2aef6dfb
Alexey Shulga <shulgaalexey@gmail.com>
2019-12-16 20:01:21 +0000

    Dont send reconnect message to the operator

--- rpi/slackbot.py
+++ rpi/slackbot.py
@@ -216,11 +216,11 @@ try:
             _log('Slack connect socket error, reconnect in 15 sec, reason: "%s"' % sce)
             time.sleep(15)
             slack_client = connect()
-            if slack_client:
-                post_message(slack_client, 'Reconnect after exception\n```%s```' % sce, CUR_CHANNEL)
-                time.sleep(2)
-            else:
-                _log('Unable to connect to slack')
+            #if slack_client:
+            #    post_message(slack_client, 'Reconnect after exception\n```%s```' % sce, CUR_CHANNEL)
+            #    time.sleep(2)
+            #else:
+            _log('Unable to connect to slack')
         except socket.error as se:
             # _log('Socket error, reconnect in 15 sec, reason: "%s"' % se)
             time.sleep(15)
@@ -229,7 +229,7 @@ try:
             #    post_message(slack_client, 'Reconnect after exception\n```%s```' % se, CUR_CHANNEL)
             #    time.sleep(2)
             #else:
-            #    _log('Unable to connect to slack')
+            _log('Unable to connect to slack')
         except IOError as ex:
             _log('Cannot fetch messages: %s' % ex)
             post_message(slack_client, 'Exception\n```%s```' % ex, CUR_CHANNEL)
